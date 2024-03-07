473a67d882a90d04b6c828c04b13c76adc02e65a
aardnsyhs <ardiansyahdian755@gmail.com>
2024-03-07 13:38:29 +0700

    menyelesaikan usia yang 0 di tabel kematian

--- pages/kematian/index.php
+++ pages/kematian/index.php
@@ -28,25 +28,34 @@
                     include "config/connection.php";
                     $no = 1;
                     $sql = $koneksi->query("SELECT * FROM `penduduk` JOIN `surat_kematian` ON `penduduk`.id_penduduk = `surat_kematian`.id_penduduk");
-                    while ($data = $sql->fetch_assoc()) {
+                    while ($row = mysqli_fetch_assoc($sql)) {
+                        $data_kematian[] = $row;
+                    }
+                    function hitungUsia($tanggal_lahir, $tanggal_kematian)
+                    {
+                        $lahir = new DateTime($tanggal_lahir);
+                        $kematian = new DateTime($tanggal_kematian);
+                        $usia = $lahir->diff($kematian);
+                        return $usia->y;
+                    }
+                    $tanggal_lahir = $data_kematian[0]['tanggal_lahir_penduduk'];
+                    $tanggal_kematian = $data_kematian[0]['tanggal_kematian'];
                     ?>
-                        <tr>
-                            <td><?= $no ?></td>
-                            <td><?= $data['nik_penduduk'] ?></td>
-                            <td><?= $data['nama_penduduk'] ?></td>
-                            <td><?= $data['jenis_kelamin_penduduk'] ?></td>
-                            <td><?= $data['usia_kematian'] ?></td>
-                            <td><?= $data['penyebab_kematian'] ?></td>
-                            <td>
-                                <a href="?page=detail_kematian&id_penduduk=<?= $data['id_penduduk']; ?>" title="Detail" class="btn btn-info btn-sm">
-                                    <i class="fa fa-user"></i>
-                                </a>
-                                <a href="?page=edit_kematian&id_penduduk=<?= $data['id_penduduk']; ?>" title="Ubah" class="btn btn-success btn-sm">
-                                    <i class="fa fa-edit"></i>
-                                </a>
-                            </td>
-                        </tr>
-                    <?php } ?>
+                    <tr>
+                        <td><?= $no ?></td>
+                        <td><?= $data_kematian[0]['nik_penduduk'] ?></td>
+                        <td><?= $data_kematian[0]['nama_penduduk'] ?></td>
+                        <td><?= $data_kematian[0]['jenis_kelamin_penduduk'] ?></td>
+                        <td><?= hitungUsia($tanggal_lahir, $tanggal_kematian) ?></td>
+                        <td><?= $data_kematian[0]['penyebab_kematian'] ?></td>
+                        <td>
+                            <a href="?page=detail_kematian&id_penduduk=<?= $data_kematian[0]['id_penduduk']; ?>" title="Detail" class="btn btn-info btn-sm">
+                                <i class="fa fa-user"></i>
+                            </a>
+                            <a href="?page=edit_kematian&id_penduduk=<?= $data_kematian[0]['id_penduduk']; ?>" title="Ubah" class="btn btn-success btn-sm">
+                                <i class="fa fa-edit"></i>
+                            </a>
+                        </td>
                 </tbody>
                 </tfoot>
             </table>
